c233fa12cd97e9eaa33adf52213e040e650c1b5b
ZhangXudong <zhangxudong@greatwall.com.cn>
2018-08-27 10:26:57 +0800

    修改FPGA基地址偏移读写操作处理(SOCPLATFORM-3)

--- fw_platform/fpga/fpga.c
+++ fw_platform/fpga/fpga.c
@@ -18,14 +18,14 @@ volatile uint32_t *ints_reg_base;
 uint32_t fpga_get_version(void)
 {
 	uint32_t version;
-	fpga_readl(&version, ctrl_reg_base + FPGA_REG_VERSION);
+	fpga_readl(&version, (char *)ctrl_reg_base + FPGA_REG_VERSION);
 	return version;
 }
 
 static int  fpga_is_ready(void)
 {
 	uint32_t ready;
-	fpga_readl(&ready, ctrl_reg_base + FPGA_REG_STATUS);
+	fpga_readl(&ready, (char *)ctrl_reg_base + FPGA_REG_STATUS);
 	return ready;
 
 }
@@ -35,28 +35,28 @@ void fpga_reset(void)
 	uint32_t int_status;
 
 	// disable all FPGA interrupts(enale global only)
-	fpga_writel(FPGA_REG_INT_SEL_ALL, ints_reg_base + FPGA_REG_INT_ENABLE);
+	fpga_writel(FPGA_REG_INT_SEL_ALL, (char *)ints_reg_base + FPGA_REG_INT_ENABLE);
 	// clear all pending FPGA interrupts
-	fpga_readl(&int_status, ints_reg_base + FPGA_REG_INT_STATUS);
-	fpga_writel(int_status, ints_reg_base + FPGA_REG_INT_CLEAR);
+	fpga_readl(&int_status, (char *)ints_reg_base + FPGA_REG_INT_STATUS);
+	fpga_writel(int_status, (char *)ints_reg_base + FPGA_REG_INT_CLEAR);
 }
 
 
 void fpga_enable_interrupt(uint32_t mask)
 {
-	fpga_update_lbits(ints_reg_base + FPGA_REG_INT_ENABLE, mask, mask);
+	fpga_update_lbits((char *)ints_reg_base + FPGA_REG_INT_ENABLE, mask, mask);
 }
 
 
 void fpga_disable_interrupt(uint32_t mask)
 {
-	fpga_update_lbits(ints_reg_base + FPGA_REG_INT_ENABLE, mask, 0);
+	fpga_update_lbits((char *)ints_reg_base + FPGA_REG_INT_ENABLE, mask, 0);
 }
 
 
 void fpga_clear_interrupt(uint32_t mask)
 {
-	fpga_writel(mask, ints_reg_base + FPGA_REG_INT_CLEAR);
+	fpga_writel(mask, (char *)ints_reg_base + FPGA_REG_INT_CLEAR);
 }
 
 
