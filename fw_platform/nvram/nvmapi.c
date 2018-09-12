e1a33ade66126dc415970b9da218c51ba415adf8
ZhangXudong <zhangxudong@greatwall.com.cn>
2018-09-12 16:01:56 +0800

    增加nvram API模块(SOCPLATFORM-11)

---
+++ fw_platform/nvram/nvmapi.c
@@ -0,0 +1,260 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "nvram.h"
+#include "nvmapi.h"
+
+
+#define DEBUG	1
+#undef DEBUG
+
+
+static nvram_control_t *nvmsect_table = NULL;
+static int nvmsect_num = 0;
+
+extern uint32_t crc32( const unsigned char *buf, uint32_t size);
+
+static int id_to_block_index(int id, nvram_block_t *block_table, int block_num);
+static int calc_offset(nvram_block_t *block_table, int block_num);
+
+
+int calc_offset(nvram_block_t *block_table, int block_num)
+{
+	int i;
+
+	block_table[0].offset = 0;
+	for(i = 1; i < block_num; i++)
+		block_table[i].offset = block_table[i - 1].offset + block_table[i - 1].info->length;
+
+	return block_table[i-1].offset + block_table[i-1].info->length;
+}
+
+
+int id_to_block_index(int id, nvram_block_t *block_table, int block_num)
+{
+	int i;
+
+	for (i = 0; i < block_num; i++)
+		if (block_table[i].id == id)
+			return i;
+	return -1;
+}
+
+
+/*
+ *  nvram section register
+ */
+int nvm_register(int sections, nvram_control_t *nvm_sect)
+{
+
+	if (sections < 0 || nvm_sect == NULL)
+		return -1;
+
+	nvmsect_table = nvm_sect;
+	nvmsect_num = sections;
+
+	return 0;
+}
+
+
+/*
+ *  nvram management initialize
+ */
+int nvm_initialize(int section)
+{
+	nvram_control_t *nvmsect;
+	nvram_data_t *nvmdata;
+	struct nvram *nvm;
+	uint8_t *buffer;
+	int count;
+	int rs;
+
+	if (!nvmsect_table || section > nvmsect_num)
+		return -1;
+
+	nvmsect = &nvmsect_table[section];
+	nvmsect->nvm = NULL;
+	nvm = nvram_get(nvmsect->dev_id);
+	if (!nvm)
+		return -1;
+
+	nvmsect->nvm = nvm;
+
+	count = calc_offset(nvmsect->nvmblock, nvmsect->blocks);
+	nvmsect->datacnt = count;
+	count += sizeof(nvram_data_t);	/* add nvram data head */
+	if (count > nvmsect->length)
+		return -1;
+
+	buffer = malloc(count);
+	if (!buffer)
+		return -1;
+
+	nvmsect->buflen = count;
+	nvmsect->buffer = buffer;
+	nvram_read(nvm, buffer, nvmsect->offset, nvmsect->buflen);
+
+	rs = 0;
+	nvmdata = (nvram_data_t *)buffer;
+	if (nvmdata->validflag != nvmsect->validflag)
+		rs = NVM_ERR_VALIDFLAG;
+	else if (nvmdata->version != nvmsect->version)
+		rs = NVM_ERR_VERSION;
+	else if (nvmdata->length != nvmsect->datacnt)
+		rs = NVM_ERR_LENGTH;
+	else if (nvmdata->checksum != crc32(nvmdata->data, nvmdata->length))
+		rs = NVM_ERR_CHECKSUM;
+	if (rs != 0)
+	{
+		nvmdata->validflag = nvmsect->validflag;
+		nvmdata->version = nvmsect->version;
+		nvmdata->length = nvmsect->datacnt;
+		memset(nvmdata->data, 0xff, nvmdata->length);	// set data area to blank data 0xFF
+
+		nvmsect->flags = NVM_FLAG_DATA_INVALID;
+	}
+	else
+		nvmsect->flags = NVM_FLAG_NORMAL;
+
+	return rs;
+}
+
+
+/*
+ *  nvram management resource release
+ */
+void nvm_release(void)
+{
+	int i;
+	for (i = 0; i < nvmsect_num; i++)
+	{
+		free(nvmsect_table[i].buffer);
+		nvmsect_table[i].buffer = NULL;
+	}
+}
+
+
+/*
+ *  set default value of a nvram data section
+ */
+int nvm_setdefault(int section)
+{
+	nvram_control_t *nvmsect;
+	nvram_block_t *nvmblock;
+	nvram_data_t *nvmdata;
+	uint32_t chksum;
+	int i;
+
+	if (!nvmsect_table || section > nvmsect_num)
+		return -1;
+
+	nvmsect = &nvmsect_table[section];
+	nvmblock = nvmsect->nvmblock;
+	nvmdata = (nvram_data_t *)nvmsect->buffer;
+	for (i=0; i < nvmsect->blocks; i++) {
+		uint8_t *dataptr = &(nvmdata->data[nvmblock[i].offset]);
+		if (nvmblock[i].info->defaultdata)
+			memcpy(dataptr, nvmblock[i].info->defaultdata, nvmdata->length);
+	}
+	chksum = crc32(nvmdata->data, nvmdata->length);
+	if (chksum != nvmdata->checksum) {
+		nvmdata->checksum = chksum;
+		nvram_write(nvmsect->nvm, nvmsect->buffer, nvmsect->offset, nvmsect->buflen);
+	}
+
+	return 0;
+}
+
+
+/*
+ *  write data block to nvram
+ */
+int nvm_write(int section, int block_id, void *buffer, int count)
+{
+	nvram_control_t *nvmsect;
+	nvram_block_t *nvmblock;
+	nvram_data_t *nvmdata;
+	uint8_t *dataptr;
+	int index;
+
+	if (!nvmsect_table || section > nvmsect_num)
+		return -1;
+
+	nvmsect = &nvmsect_table[section];
+	index = id_to_block_index(block_id, nvmsect->nvmblock, nvmsect->blocks);
+	if (index == -1)
+		return -1;
+
+	nvmblock = &(nvmsect->nvmblock[index]);
+	if (count > nvmblock->info->length)
+		count = nvmblock->info->length;
+
+	nvmdata = (nvram_data_t *)nvmsect->buffer;
+	dataptr = &(nvmdata->data[nvmblock->offset]);
+	if (memcmp(dataptr, buffer, count) != 0)
+	{
+		uint32_t chksum;
+		memcpy(dataptr, buffer, count);
+		chksum = crc32(nvmdata->data, nvmdata->length);
+		if (chksum != nvmdata->checksum) {
+			nvmdata->checksum = chksum;
+			nvmsect->flags = NVM_FLAG_FLUSH_PENDING;
+			// TODO: change to delayed nvram writing operation
+			nvram_write(nvmsect->nvm, nvmsect->buffer, nvmsect->offset, nvmsect->buflen);
+			nvmsect->flags &= ~NVM_FLAG_FLUSH_PENDING;
+		}
+	}
+	return count;
+}
+
+
+/*
+ *  read data block from nvram
+ */
+int nvm_read(int section, int block_id, void *buffer, int count)
+{
+	nvram_control_t *nvmsect;
+	nvram_block_t *nvmblock;
+	nvram_data_t *nvmdata;
+	uint8_t *dataptr;
+	int index;
+
+	if (!nvmsect_table || section > nvmsect_num)
+		return -1;
+
+	nvmsect = &nvmsect_table[section];
+	index = id_to_block_index(block_id, nvmsect->nvmblock, nvmsect->blocks);
+	if (index == -1)
+		return -1;
+
+	nvmblock = &(nvmsect->nvmblock[index]);
+	if (count > nvmblock->info->length)
+		count = nvmblock->info->length;
+
+	nvmdata = (nvram_data_t *)nvmsect->buffer;
+	dataptr = &(nvmdata->data[nvmblock->offset]);
+	memcpy(buffer, dataptr, count);
+
+	return count;
+}
+
+
+/*
+ *  flush all pennding nvram data writing
+ */
+void nvm_flush(void)
+{
+	int i;
+	for (i=0; i<nvmsect_num; i++) {
+		nvram_control_t *nvmsect;
+		nvmsect = &nvmsect_table[i];
+		if (nvmsect_table[i].flags & NVM_FLAG_FLUSH_PENDING)
+		{
+			nvram_write(nvmsect->nvm, nvmsect->buffer, nvmsect->offset, nvmsect->buflen);
+			nvmsect->flags &= ~NVM_FLAG_FLUSH_PENDING;
+		}
+	}
+}
+
---
+++ include/nvmapi.h
@@ -0,0 +1,92 @@
+#ifndef __NVMAPI_H__
+#define __NVMAPI_H__
+
+#include<stdint.h>
+#define MAX_NAME_LEN	256
+
+#pragma pack(1)		// set one-byte alignment to save space in NVRAM
+
+// data structure of nvram data
+typedef struct nvram_data_s
+{
+	uint16_t validflag;	// validate flag
+	uint16_t version;	// nvram data profile version
+	int length;		// nvram data length
+	uint32_t checksum;	// CRC-32 checksum
+	uint8_t data[];		// application data
+} nvram_data_t;
+
+#pragma pack()		// restore default alignment
+
+
+// data structure of nvram data information
+typedef struct nvmdata_info_s
+{
+	const int length;
+	const void *const defaultdata;
+} nvmdata_info_t;
+
+
+// data structure of nvram block table
+typedef struct nvram_block_s
+{
+	int offset;			// offset in nvram application data
+	const nvmdata_info_t *const info; 	// pointer to nvram data information
+	const int id;			// nvram block id
+	const char *name;		// nvram block name
+} nvram_block_t;
+
+
+// data structure of nvram control block
+typedef struct nvram_control_s
+{
+	const int dev_id;		// nvram device ID
+	const uint16_t validflag;	// validate flag
+	const uint16_t version;		// nvram data profile version
+	const int offset;		// memory region offset in the nvram device
+	const int length;		// memory region length
+	const int blocks;		// number of data blocks in the nvram section
+	nvram_block_t *const nvmblock;	// pointer to nvram block table
+	int datacnt;			// nvram user data count
+	int flags;			// nvram management flags
+	struct nvram *nvm;		// pointer to nvram device
+	unsigned char *buffer;		// pointer to RAM copy buffer of nvram data block
+	int buflen;			// length of RAM copy buffer
+} nvram_control_t;
+
+
+/* nvram flags definition */
+#define NVM_FLAG_NORMAL		0
+#define NVM_FLAG_DATA_INVALID	0x01
+#define NVM_FLAG_FLUSH_PENDING	0x02
+
+
+/* nvram data errors definition */
+#define NVM_ERR_VALIDFLAG	-11
+#define NVM_ERR_VERSION		-12
+#define NVM_ERR_LENGTH		-13
+#define NVM_ERR_CHECKSUM	-14
+
+
+/* macros of reference to nvram data ID, name, information variable */
+#define NVM_DATA_NAME(name)	#name
+#define NVM_DATA_ID(name)	NVMDATA_##name
+#define NVM_DATA_INFO(name)	nvmdata_info_##name
+
+/* macro to define and initialize a nvram data information variable */
+#define DEFINE_NVM_DATA(name, len, defval)	const nvmdata_info_t NVM_DATA_INFO(name) = {(len), (defval)}
+
+/* macro to declare a nvram data information variable */
+#define DECLARE_NVM_DATA(name)			extern const nvmdata_info_t NVM_DATA_INFO(name)
+
+
+/* function prototypes */
+int nvm_register(int sections, nvram_control_t *nvm_sect);
+int nvm_initialize(int section);
+void nvm_release(void);
+void nvm_flush(void);
+int nvm_setdefault(int section);
+int nvm_write(int section, int block_id, void *buffer, int count);
+int nvm_read(int section, int block_id, void *buffer, int count);
+
+#endif /* __NVMAPI_H__ */
