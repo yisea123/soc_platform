87086796d16c739d62767adb398678e407b6562e
hl <huling@greatwall.com.cn>
2018-10-24 14:01:43 +0800

    增加GPIO按键设备驱动程序(SOCPLATFORM-14)

---
+++ fw_platform/gpiokey/gpiokey.c
@@ -0,0 +1,96 @@
+#include "gpiokey.h"
+
+extern struct gpiokey gpiokey_list[];
+extern const int gpiokey_num ;
+
+static void gpiokey_irq_handler(void *device, int gpio, void *data)
+{
+  	struct gpiokey_resource *pgpiokey_rc = (struct gpiokey_resource *)data;
+	pgpiokey_rc->gpiokey_status = GPIOKEY_ST_ACTIVE;
+}
+
+void gpiokey_init(struct gpiokey_resource *pgpiokey_rc)
+{
+	pgpiokey_rc->gpiokey_status = GPIOKEY_ST_NOT_ACTIVE;
+	
+	if (!pgpiokey_rc->gpiochip)	//  MSS GPIO instance
+	{
+		MSS_GPIO_disable_irq((mss_gpio_id_t)pgpiokey_rc->gpio);
+		MSS_GPIO_clear_irq((mss_gpio_id_t)pgpiokey_rc->gpio);
+		
+		switch(pgpiokey_rc->gpiokey_type)
+		{
+		case GPIOKEY_TYPE_IRQ_EDGE_LEVEL_HIGH:
+		  	MSS_GPIO_config((mss_gpio_id_t)pgpiokey_rc->gpio, MSS_GPIO_INPUT_MODE|MSS_GPIO_IRQ_LEVEL_HIGH);
+			break;
+		case GPIOKEY_TYPE_IRQ_EDGE_LEVEL_LOW:
+		  	MSS_GPIO_config((mss_gpio_id_t)pgpiokey_rc->gpio, MSS_GPIO_INPUT_MODE|MSS_GPIO_IRQ_LEVEL_LOW);
+		 	break;
+		case GPIOKEY_TYPE_IRQ_EDGE_POSITIVE:		  
+		  	MSS_GPIO_config((mss_gpio_id_t)pgpiokey_rc->gpio, MSS_GPIO_INPUT_MODE|MSS_GPIO_IRQ_EDGE_POSITIVE);
+		  	break;
+		case GPIOKEY_TYPE_IRQ_EDGE_NEGATIVE:
+			MSS_GPIO_config((mss_gpio_id_t)pgpiokey_rc->gpio, MSS_GPIO_INPUT_MODE|MSS_GPIO_IRQ_EDGE_NEGATIVE);
+			break;
+		case GPIOKEY_TYPE_IRQ_EDGE_BOTH:
+		  	MSS_GPIO_config((mss_gpio_id_t)pgpiokey_rc->gpio, MSS_GPIO_INPUT_MODE|MSS_GPIO_IRQ_EDGE_BOTH);
+		  	break;
+		default:
+		  	return;
+		}
+		mss_gpio_irqcallback_install((mss_gpio_id_t)pgpiokey_rc->gpio, (irqcallback_t)gpiokey_irq_handler, (void *)pgpiokey_rc);
+
+		MSS_GPIO_enable_irq((mss_gpio_id_t)pgpiokey_rc->gpio);
+		
+	}
+	else				//  CoreGPIO instance
+	{
+		GPIO_disable_irq(pgpiokey_rc->gpiochip, (gpio_id_t)pgpiokey_rc->gpio);
+		GPIO_clear_irq(pgpiokey_rc->gpiochip, (gpio_id_t)pgpiokey_rc->gpio);
+		
+		GPIO_config(pgpiokey_rc->gpiochip, (gpio_id_t)pgpiokey_rc->gpio, GPIO_INPUT_MODE|GPIO_IRQ_EDGE_NEGATIVE);
+		GPIO_enable_irq(pgpiokey_rc->gpiochip, (gpio_id_t)pgpiokey_rc->gpio);
+		// TODO: install GPIO irqcallback here.
+	}
+}
+
+int gpiokey_install_devices(void)
+{
+	int i, rs, dev_err;
+
+	dev_err = 0;
+	for (i=0; i<gpiokey_num; i++)
+	{
+		if (gpiokey_list[i].install != NULL)
+		{
+			rs = gpiokey_list[i].install(&gpiokey_list[i]);
+			if (rs == 0)
+				continue;
+		}
+		++dev_err;
+	}
+	return -dev_err;
+}
+
+int gpiokey_is_active(struct gpiokey *pgpiokey)
+{
+  return ((((struct gpiokey_resource *)pgpiokey->resource)->gpiokey_status == GPIOKEY_ST_ACTIVE)?1:0);
+}
+
+void gpiokey_status_clear(struct gpiokey *pgpiokey)
+{
+	((struct gpiokey_resource *)pgpiokey->resource)->gpiokey_status=GPIOKEY_ST_NOT_ACTIVE;
+}
+
+int gpiokey_install(struct gpiokey *pgpiokey)
+{
+	if (!pgpiokey)
+		return -1;
+	if (!pgpiokey->resource)
+		return -1;
+
+	pgpiokey->status_is_active = gpiokey_is_active;
+	pgpiokey->status_clear = gpiokey_status_clear;
+
+	return 0;
+}
---
+++ fw_platform/gpiokey/gpiokey.h
@@ -0,0 +1,40 @@
+#ifndef __GPIOKEY_H__
+#define __GPIOKEY_H__
+
+#include "core_gpio.h"
+#include "mss_gpio.h"
+#include "irqcallback.h"
+
+/* low-lever hardware gpio status mapping to logical status */
+typedef enum {
+  	GPIOKEY_TYPE_IRQ_EDGE_LEVEL_HIGH,
+	GPIOKEY_TYPE_IRQ_EDGE_LEVEL_LOW,	
+	GPIOKEY_TYPE_IRQ_EDGE_POSITIVE,
+	GPIOKEY_TYPE_IRQ_EDGE_NEGATIVE,
+	GPIOKEY_TYPE_IRQ_EDGE_BOTH,
+} gpiokey_type_t;
+
+typedef enum {
+  	GPIOKEY_ST_NOT_ACTIVE,
+  	GPIOKEY_ST_ACTIVE,
+} gpiokey_status_t;
+
+struct gpiokey_resource {
+	// resource of input GPIO
+	gpio_instance_t *gpiochip;	// pointer to a GPIO chip instance. Should be NULL if using MSS_GPIO.
+	int gpio;			// GPIO port of a GPIO chip
+	gpiokey_type_t gpiokey_type;
+	gpiokey_status_t gpiokey_status;
+};
+
+struct gpiokey {
+	const void *resource;				// pointer to the resource block of an imagesensor instance
+	int (*const install)(struct gpiokey *key);	// pointer to the device installation function
+	int (*status_is_active)(struct gpiokey *key);
+	void (*status_clear)(struct gpiokey *key);
+};
+
+extern int gpiokey_install_devices(void);
+extern int gpiokey_install(struct gpiokey *pgpiokey);
+
+#endif
