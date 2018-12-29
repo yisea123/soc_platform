77bbb43ad72a96a8b4d2dd32623e1dce45029d84
hl <huling@greatwall.com.cn>
2018-12-29 14:41:22 +0800

    更新平台脚本，增加bootloader的工程链接(SOCPLATFORM-20)

--- scripts/makelinks.bat
+++ scripts/makelinks.bat
@@ -8,12 +8,15 @@ set product_root=%cd%
 set firmware_root=%product_root%\firmware
 cd ..
 set platform_root=%cd%\platform
+set bootloader_root=%cd%\bootloader
 
 echo platform_root = %platform_root%
+echo bootloader_root = %bootloader_root%
 echo product_root = %product_root%
 echo firmware_root = %firmware_root%
 
 if not exist %platform_root% goto noplatform
+if not exist %bootloader_root% goto nobootloader
 
 REM make links to include, FreeRTOS, fw_platform
 cd %firmware_root%
@@ -21,6 +24,11 @@ echo %cd%
 if not exist include mklink /D include %platform_root%\include
 if not exist FreeRTOS mklink /D FreeRTOS %platform_root%\FreeRTOS
 if not exist fw_platform mklink /D fw_platform %platform_root%\fw_platform
+
+REM make links to bootloader
+cd %firmware_root%
+echo %cd%
+if not exist bootloader mklink /D bootloader %bootloader_root%\application
 
 REM make links in hw_platform
 cd %firmware_root%\hw_platform
@@ -35,12 +43,21 @@ echo %cd%
 if not exist freertos.ewp mklink freertos.ewp %platform_root%\project\IAR-EWARM\freertos.ewp
 if not exist firmware_platform.ewp mklink firmware_platform.ewp %platform_root%\project\IAR-EWARM\firmware_platform.ewp
 if not exist hardware_platform.ewp mklink hardware_platform.ewp %platform_root%\project\IAR-EWARM\hardware_platform.ewp
+if not exist bootloader.eww mklink /H bootloader.eww %bootloader_root%\project\IAR-EWARM\bootloader.eww
+if not exist bootloader.ewp mklink /H bootloader.ewp %bootloader_root%\project\IAR-EWARM\bootloader.ewp
+if not exist bootloader.ewd mklink bootloader.ewd %bootloader_root%\project\IAR-EWARM\bootloader.ewd
+if not exist smartfusion2_envm_bootloader.icf mklink smartfusion2_envm_bootloader.icf %bootloader_root%\project\IAR-EWARM\smartfusion2_envm_bootloader.icf
+
 
 goto end
 
 :noplatform
 echo SOC firmware platform directory is not found !
 echo Please make sure the platform directory is in the SAME directory of the product source root !
+
+:nobootloader
+echo SOC firmware bootloader directory is not found !
+echo Please make sure the bootloader directory is in the SAME directory of the product source root !
 
 :end
 cd %firmware_root%
