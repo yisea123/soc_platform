63980d9099b88eeeb209780f6dead74176120fa3
ZhangXudong <zhangxudong@greatwall.com.cn>
2019-05-24 14:46:37 +0800

    增加通用模数转换器ADC设备驱动抽象层(SOCPLATFORM-28)

---
+++ fw_platform/sensor/adc.c
@@ -0,0 +1,135 @@
+/*
+ * Analog-to-Digital Converter (ADC) driver (device-independant abstraction layer)
+ *
+ * Copyright 2019 Hunan GreatWall Information Financial Equipment Co., Ltd.
+ *
+ */
+#include "adc.h"
+
+
+int adc_install_devices(void)
+{
+	int i, rs, dev_err;
+
+	dev_err = 0;
+	for (i=0; i<adconverter_num; i++)
+	{
+		if (adconverter_list[i]->install != NULL)
+		{
+			rs = adconverter_list[i]->install(adconverter_list[i]);
+			if (rs == 0)
+				continue;
+		}
+		++dev_err;
+	}
+	return -dev_err;
+}
+
+
+struct ad_converter *adc_get(int index)
+{
+	if (index >= adconverter_num || index < 0)
+		return NULL;
+	else
+		return adconverter_list[index];
+}
+
+
+int adc_init(struct ad_converter *adc, void *data)
+{
+	if (adc || adc->channels > MAX_ADC_CHANNELS)
+		return adc->init(adc, data);
+
+	return -1;
+}
+
+
+int adc_status(struct ad_converter *adc, uint32_t *status)
+{
+	if (adc)
+		return adc->status(adc, status);
+
+	return -1;
+}
+
+
+void adc_enable(struct ad_converter *adc, int channel)
+{
+	if (adc)
+		adc->enable(adc, channel);
+}
+
+
+void adc_disable(struct ad_converter *adc, int channel)
+{
+	if (adc)
+		adc->disable(adc, channel);
+}
+
+
+int adc_read_raw(struct ad_converter *adc, int channel, uint32_t *value)
+{
+	if (!adc || channel < 0 || channel >= adc->channels)
+		return -1;
+
+	return adc->read_raw(adc, channel, value);
+}
+
+
+int adc_read_average(struct ad_converter *adc, int channel, uint32_t *value)
+{
+	if (!adc || channel < 0 || channel >= adc->channels)
+		return -1;
+
+	return adc->read_average(adc, channel, value);
+}
+
+
+int adc_read_compare(struct ad_converter *adc, int channel, uint32_t *value)
+{
+	if (!adc || channel < 0 || channel >= adc->channels)
+		return -1;
+
+	return adc->read_compare(adc, channel, value);
+}
+
+
+int adc_set_threshold(struct ad_converter *adc, int channel, uint32_t value)
+{
+	if (!adc || channel < 0 || channel >= adc->channels)
+		return -1;
+
+	return adc->set_threshold(adc, channel, value);
+}
+
+
+int adc_set_event(struct ad_converter *adc, int channel, adc_event_t event)
+{
+	if (!adc || channel < 0 || channel >= adc->channels)
+		return -1;
+
+	adc->event[channel] = event;
+	return adc->set_event(adc, channel, event);
+}
+
+
+int adc_unset_event(struct ad_converter *adc, int channel)
+{
+	if (!adc || channel < 0 || channel >= adc->channels)
+		return -1;
+
+	adc->event[channel] = ADC_EVT_NONE;
+	return adc->unset_event(adc, channel);
+}
+
+
+int adc_set_callback(struct ad_converter *adc, int channel, void (*callback)(void *, int, void *), void *data)
+{
+	if (!adc || channel < 0 || channel >= adc->channels)
+		return -1;
+
+	adc->callback[channel] = callback;
+	adc->callbackdata[channel] = data;
+
+	return 0;
+}
---
+++ fw_platform/sensor/adc.h
@@ -0,0 +1,87 @@
+/*
+ * Analog-to-Digital Converter (ADC) device driver definitions
+ *
+ * Copyright 2019 Hunan GreatWall Information Financial Equipment Co., Ltd.
+ *
+ */
+#ifndef __ADC_H__
+#define __ADC_H__
+
+#include <stddef.h>
+#include <stdint.h>
+
+
+#define MAX_ADC_CHANNELS	24
+
+
+/* generic ADC events */
+typedef enum {
+	ADC_EVT_NONE,
+	ADC_EVT_CONVERSION_DONE,
+	ADC_EVT_CHANGE_RISING,
+	ADC_EVT_CHANGE_FALLING,
+	ADC_EVT_CHANGE_EITHER,
+} adc_event_t;
+
+
+/*  ADC device control types () */
+typedef enum {
+	ADC_CTRL_SOFTWARE,	/* ADC device is controlled by software */
+	ADC_CTRL_HARDWARE,	/* ADC device is controlled by hardware (typically FPGA logic) */
+} adc_ctrl_t;
+
+
+/*
+ * struct ad_converter
+ *
+ * One for each ADC device.
+ */
+struct ad_converter {
+	const void *resource;				// pointer to the resource block of an ADC instance
+	int (*const install)(struct ad_converter *adc);	// pointer to the device installation function
+	int channels;					// number of channels of an ADC device
+	adc_ctrl_t ctrltype;				// ADC device control type
+
+	/* ADC event callback definition */
+	void (*callback[MAX_ADC_CHANNELS])(void *adc, int channel, void *data);	// callback handling ADC events
+	adc_event_t event[MAX_ADC_CHANNELS];
+	void *callbackdata[MAX_ADC_CHANNELS];
+
+	/* ADC operation functions */
+	int (*init)(struct ad_converter *adc, void *data);
+	void (*enable)(struct ad_converter *adc, int channel);
+	void (*disable)(struct ad_converter *adc, int channel);
+	int (*status)(struct ad_converter *adc, uint32_t *status);
+	int (*read_raw)(struct ad_converter *adc, int channel, uint32_t *value);
+	int (*read_average)(struct ad_converter *adc, int channel, uint32_t *value);
+	int (*read_compare)(struct ad_converter *adc, int channel, uint32_t *value);
+	int (*set_threshold)(struct ad_converter *adc, int channel, uint32_t value);
+	int (*set_event)(struct ad_converter *adc, int channel, adc_event_t event);
+	int (*unset_event)(struct ad_converter *adc, int channel);
+};
+
+
+/* declaration of ADC list */
+extern struct ad_converter * adconverter_list[];
+extern const int adconverter_num;
+
+
+/* function prototypes of ADC driver */
+extern struct ad_converter *adc_get(int index);
+extern int adc_install_devices(void);
+
+extern int adc_init(struct ad_converter *adc, void *data);
+extern void adc_enable(struct ad_converter *adc, int channel);
+extern void adc_disable(struct ad_converter *adc, int channel);
+extern int adc_status(struct ad_converter *adc, uint32_t *status);
+extern int adc_read_raw(struct ad_converter *adc, int channel, uint32_t *value);
+extern int adc_read_average(struct ad_converter *adc, int channel, uint32_t *value);
+extern int adc_read_compare(struct ad_converter *adc, int channel, uint32_t *value);
+extern int adc_set_threshold(struct ad_converter *adc, int channel, uint32_t value);
+
+extern int adc_set_event(struct ad_converter *adc, int channel, adc_event_t event);
+extern int adc_unset_event(struct ad_converter *adc, int channel);
+extern int adc_set_callback(struct ad_converter *adc, int channel, void (*callback)(void *, int, void *), void *data);
+
+
+#endif /* __ADC_H__ */
